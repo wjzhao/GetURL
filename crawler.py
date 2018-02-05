ba787d9fea0911497a27369e618831f6f85ba659
zhaoweijie <zhaoweijie@innobuddy.com>
2018-02-05 10:53:55 +0800

    尚未完成

--- crawler.py
+++ crawler.py
@@ -9,8 +9,10 @@ processed = []
 
 def search_links(url, depth, keyword):
     url_is_processed = (url in processed)
-    if (not url_is_processed):
+    if (url.startswith("http://") and (not url_is_processed)):
         processed.append(url)
+        url = host = url.replace("http://", "", 1)
+        path = "/"
 
         urlparts = url.split('/')
         if (len(urlparts) > 1):
@@ -22,4 +24,34 @@ def search_links(url, depth, keyword):
         req = conn.request("GET", path)
         result = conn.getresponse()
 
-        
+        contents = result.read().decode('utf-8')
+        pattern = re.compile(r'href="(.*?)"')
+        all_links = pattern.findall(contents)
+
+        if (keyword in contents):
+            print("Found %s at %s" %(keyword, url))
+
+        print("===> %s: processing %s links" %(str(depth), str(len(all_links))))
+        for href in all_links:
+            if (href.startswith("/")):
+                href = "http://%s%s" %(host, href)
+
+            if (depth > 0):
+                search_links(href, depth-1, href)
+
+    else:
+        print("Skipping link: %s" %url)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Webpage link crawler")
+    parser.add_argument('--url', action="store", dest="url", required=True)
+    parser.add_argument('--query', action="store", dest="query", required=True)
+    parser.add_argument('--depth', action="store", dest="depth", default=3)
+
+    given_args = parser.parse_args()
+
+    try:
+        search_links(given_args.url, given_args.depth, given_args.query)
+    except KeyboardInterrupt:
+        print("Aborting search by user request")
